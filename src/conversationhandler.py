599bcec0fb6caeb157e7607b7e016bd287aceee9
Bruno <fracassobruno@gmail.com>
2026-06-05 13:57:16 -0300

    Changed telegram api call template to conversationhandler and set up basic flow

---
+++ src/conversationhandler.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# pylint: disable=unused-argument
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+First, a few callback functions are defined. Then, those functions are passed to
+the Application and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+CATEGORY,BUDGET_FLOW_DECIDE, SET_BUDGET, CATEGORY_FLOW_DECIDE, RECEIPT = range(5)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Starts the conversation and asks the user about their gender."""
+    user = update.effective_user
+    await update.message.reply_text(
+        f'Hello {user.first_name}, welcome to your expense tracker.')
+    await update.message.reply_text("Let's start by setting up your expense categories.")
+    await update.message.reply_text("What's the name of the category?")
+
+    return CATEGORY
+
+async def category(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the name of the expense category"""
+    context.user_date['category_name'] = update.message.text
+    user = update.message.from_user
+    logger.info("Category chosen by of %s: %s", user.first_name, update.message.text)
+    keyboard = [['YES','NO']]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+
+    await update.message.reply_text("Alright, do you wish to set a budget value for that?", reply_markup = reply_markup)
+        
+    return BUDGET_FLOW_DECIDE
+
+async def budget_flow_decide(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Decides where to direct the user concerning budget value"""
+    logger.info("User answered %s", update.message.text)
+
+    if update.message.text == 'YES':
+        await update.message.reply_text("How much?")
+        return SET_BUDGET
+    elif update.message.text == 'NO':
+        keyboard = [['YES','NO']]
+        reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+
+        await update.message.reply_text("Want to setup another category?", reply_markup = reply_markup)
+
+        return CATEGORY_FLOW_DECIDE
+
+
+async def set_budget(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the budget value for given category"""
+    context.user_date['budget_value'] = update.message.text
+    user = update.message.from_user
+    logger.info("Budget value typed by %s: %s", user.first_name, update.message.text)
+    keyboard = [['YES','NO']]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+
+    await update.message.reply_text("Want to setup another category?", reply_markup = reply_markup)
+        
+    return CATEGORY_FLOW_DECIDE
+
+async def category_flow_decide(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Decides where to direct the user concerning category registry"""
+    if update.message.text == 'YES':
+        await update.message.reply_text("What's the name of the category?")
+        return CATEGORY
+    elif update.message.text == 'NO':
+        await update.message.reply_text("Alright! Feel free to send a receipt whenever you want and I'll save the total value and tag it in the proper category.")
+        return RECEIPT
+
+
+async def receipt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Analyzes the receipt photo and returns summary"""
+    receipt_file = await update.message.photo[-1].get_file()
+    await receipt_file.download_to_drive("receipt.jpg")
+    await update.message.reply_text(
+        "Analyzing rececipt data..."
+    )
+    #call google api here
+
+    return RECEIPT
+
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            CATEGORY: [MessageHandler(filters.TEXT & ~filters.COMMAND, category)],
+            BUDGET_FLOW_DECIDE: [MessageHandler(filters.Regex("(?i)^(YES|NO)$"), budget_flow_decide)],
+            SET_BUDGET: [MessageHandler(filters.TEXT & ~filters.COMMAND, set_budget)],
+            CATEGORY_FLOW_DECIDE: [MessageHandler(filters.Regex("(?i)^(YES|NO)$"), category_flow_decide)],
+            RECEIPT: [MessageHandler(filters.PHOTO, receipt)]})
+
+    application.add_handler(conv_handler)
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -2,6 +2,13 @@
 # pylint: disable=unused-argument
 # This program is dedicated to the public domain under the CC0 license.
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
 """
 Simple Bot to reply to Telegram messages.
 
@@ -19,6 +26,7 @@ import logging
 
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import re
 
 # Enable logging
 logging.basicConfig(
@@ -33,35 +41,50 @@ logger = logging.getLogger(__name__)
 # Define a few command handlers. These usually take the two arguments update and
 # context.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
+    """Send a message when the command /start is issued."""    
+    #I have to check if this user is new or not before introducing him to the features
     user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
+    #Save user data into table users
 
+
+    await update.message.reply_text(
+        f'Hello {user.first_name}, welcome to your expense tracker.')
+    await update.message.reply_text("Let's start by determining your budget categories.")
+    await update.message.reply_text("Click /category to register a new category.")
+
+async def category_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Expect category name when /category is issued."""
+    category_name = update.message.text
+    await update.message.reply_text("Do you have a budget for that? Y/N")
+    if update.message.text.upper() == 'Y':
+        await update.message.reply_text("How much?")
+        category_budget = re.search('[0-9]+',update.message.text)
+        #save category data into categories table
+
+        await update.message.reply_text(f"{category_name} has a budget of U${category_budget}")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def listen(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+    user_input = update.message.text
+    return user_input
 
 
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("TOKEN").build()
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("category", category_command))
 
     # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, listen))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
